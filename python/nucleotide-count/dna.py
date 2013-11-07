0f59d64780cce04336898472b35a5f33c62686f6
Grace Wong <gwongz@gmail.com>
2013-11-07 00:44:11 -0800

    improving variable names

--- python/nucleotide-count/dna.py
+++ python/nucleotide-count/dna.py
@@ -1,25 +1,24 @@
 import re
 
 class DNA(object):
-	def __init__(self, dna):
-		self.dna = dna
+	def __init__(self, strand):
+		self.strand = strand
 		self.nucleotides = "ACGT"
 		self.counts = {}
 		for nucleotide in self.nucleotides:
 			self.counts[nucleotide] = 0
 
 	def nucleotide_counts(self):
-		for nucleotide in self.dna:
+		for nucleotide in self.strand:	
 			self.counts[nucleotide] += 1
 		return self.counts 
 	
 	def count(self, nucleotide):	
-
 		valid = re.compile('^[ACTGU][ACTGU]*$')
 		result = re.match(valid, nucleotide)
 		if not result:
 			raise ValueError, ("%s is not a nucleotide." % nucleotide)
 
 		else:
-			dna = list(self.dna)
-			return dna.count(nucleotide) if nucleotide != 'U' else 0 
+			strand = list(self.strand)
+			return strand.count(nucleotide) if nucleotide != 'U' else 0
