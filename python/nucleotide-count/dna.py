e946caf74a97a398d9040dada81c5aea9a557300
Grace Wong <gwongz@gmail.com>
2013-11-07 00:54:49 -0800

    adding Counter object to clean things up

--- python/nucleotide-count/dna.py
+++ python/nucleotide-count/dna.py
@@ -1,24 +1,17 @@
-import re
+from collections import Counter
 
 class DNA(object):
 	def __init__(self, strand):
 		self.strand = strand
-		self.nucleotides = "ACGT"
-		self.counts = {}
-		for nucleotide in self.nucleotides:
-			self.counts[nucleotide] = 0
 
 	def nucleotide_counts(self):
-		for nucleotide in self.strand:	
-			self.counts[nucleotide] += 1
-		return self.counts 
+		counter = Counter(A=0, C=0, G=0, T=0)
+		counter.update(self.strand)
+		return counter
 	
-	def count(self, nucleotide):	
-		valid = re.compile('^[ACTGU][ACTGU]*$')
-		result = re.match(valid, nucleotide)
-		if not result:
+	def count(self, nucleotide):
+		if nucleotide not in "ATCGU":
 			raise ValueError, ("%s is not a nucleotide." % nucleotide)
 
-		else:
-			strand = list(self.strand)
-			return strand.count(nucleotide) if nucleotide != 'U' else 0
+		counter = self.nucleotide_counts()
+		return counter[nucleotide]		
