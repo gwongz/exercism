a4992f2ef75d770fd8acba5b69263fa63518ee38
Grace Wong <gwongz@gmail.com>
2013-11-14 16:22:41 -0800

    improving regex and cleaning up init function

--- python/phone-number/phone.py
+++ python/phone-number/phone.py
@@ -1,23 +1,19 @@
 import re
-import string
 
 class Phone:
-	def __init__(self, number):
+	def __init__(self, digits):
+		self.number = self.clean(digits)
 
-		digits = "".join(x for x in number if x not in string.punctuation)
-		digits = "".join(digits.split())
-
+	def clean(self, digits):
+		digits = re.sub("[\D]", "", digits)
 		if len(digits) > 10 and not digits.startswith('1') or len(digits) < 10:
-			self.number = "0000000000"
+			digits = "0000000000"
 		elif len(digits) > 10 and digits.startswith('1'):
-			match = re.match('[\d]{10}', digits[1:])
-			self.number = match.group()
-		else:
-			match = re.match('[\d]{10}', digits)
-			self.number = match.group()
+			digits = digits[1:]
+		return digits 
 	
 	def area_code(self):
-		return self.number[0:3]
+		return self.number[:3]
 
 	def pretty(self):
 		area_code = self.area_code()
